5ea58c05768fb8c36d9934af1fec719ed7c31c2c
Bruno Volpe <bruno.volpe2015@gmail.com>
2024-10-28 22:45:14 -0300

    Add global exception handling and custom ResourceNotFoundException for invalid ID handling in UserService

--- src/main/java/com/volpe/CRUD_SpringBoot/entities/User.java
+++ src/main/java/com/volpe/CRUD_SpringBoot/entities/User.java
@@ -5,6 +5,8 @@ import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 // Serializable transforma o objeto em bytes para trafegar em rede, salvar em arquivos, etc. (implements Serializable)
 // Lombok é uma biblioteca que gera os métodos getters, setters, equals, hashcode, toString, etc. automaticamente
@@ -12,12 +14,13 @@ import java.io.Serializable;
 // Hibernate é uma implementação da especificação JPA
 // Annotations são metadados que fornecem informações sobre os dados do programa
 
-@Data // Gera os métodos getters, setters, equals, hashcode e toString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true) // Define que o equals e hashcode só será feito com os atributos marcados com @EqualsAndHashCode.Include
 @AllArgsConstructor // Necessário para o JPA
 @NoArgsConstructor // Necessário para o JPA
 @Entity // Define que a classe é uma entidade
 @Table(name = "tb_user") // Define o nome da tabela
+@Getter
+@Setter
 public class User implements Serializable {
     @Serial // Necessário para o Serializable
     private static final long serialVersionUID = 1L;
@@ -30,4 +33,16 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @Getter
+    @OneToMany(mappedBy = "client") // Define a relação de um para muitos
+    private List<Order> orders = new ArrayList<>();
+
+    public User(Long id, String name, String email, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
 }
--- src/main/java/com/volpe/CRUD_SpringBoot/repositories/UserRepository.java
+++ src/main/java/com/volpe/CRUD_SpringBoot/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.volpe.CRUD_SpringBoot.repositories;
 
 import com.volpe.CRUD_SpringBoot.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository // Define que esta classe é um componente de repositório (Nao e necessario colocar ela, por conta do JPA ja estar fazendo a mesma coisa)
 public interface UserRepository extends JpaRepository<User, Long> {
 }
